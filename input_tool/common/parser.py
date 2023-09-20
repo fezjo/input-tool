8ebb412a503df1d62387744bb0843adde0a62e4f
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-09-20 22:33:37 +0200

    Type Parser

--- input_tool/common/parser.py
+++ input_tool/common/parser.py
@@ -1,7 +1,7 @@
 # (c) 2014 jano <janoh@ksp.sk>
 import argparse
 from dataclasses import dataclass, field
-from typing import Any, Sequence, Type, TypeVar
+from typing import Any, Sequence, Type, TypedDict, TypeVar
 
 
 @dataclass
@@ -68,95 +68,99 @@ class ArgsTester:
     deprecated: list[Any] = field(default_factory=list)
 
 
+class ParserOptions(TypedDict, total=False):
+    action: str
+    const: Any
+    dest: str
+    default: Any
+    help: str
+    metavar: str
+    nargs: str
+    type: Type[Any]
+
+
 class Parser:
-    dest = "dest"
-    default = "default"
-    action = "action"
-    nargs = "nargs"
-    metavar = "metavar"
-    help = "help"
-    const = "const"
-    options = {
+    options: dict[str, tuple[tuple[str, ...], ParserOptions]] = {
         # file names
         "indir": (
             ("-i", "--input"),
             {
-                dest: "indir",
-                default: "test",
-                help: "directory with input files (default=test)",
+                "dest": "indir",
+                "default": "test",
+                "help": "directory with input files (default=test)",
             },
         ),
         "outdir": (
             ("-o", "--output"),
             {
-                dest: "outdir",
-                default: "test",
-                help: "directory for output and temporary files (default=test)",
+                "dest": "outdir",
+                "default": "test",
+                "help": "directory for output and temporary files (default=test)",
             },
         ),
         "progdir": (
             ("-p", "--progdir"),
             {
-                dest: "progdir",
-                default: "test/prog",
-                help: "directory where programs compile to (default=test/prog)",
+                "dest": "progdir",
+                "default": "test/prog",
+                "help": "directory where programs compile to (default=test/prog)",
             },
         ),
         "inext": (
             ("-I",),
             {
-                dest: "inext",
-                default: "in",
-                help: "extension of input files (default=in)",
+                "dest": "inext",
+                "default": "in",
+                "help": "extension of input files (default=in)",
             },
         ),
         "outext": (
             ("-O",),
             {
-                dest: "outext",
-                default: "out",
-                help: "extension of output files (default=out)",
+                "dest": "outext",
+                "default": "out",
+                "help": "extension of output files (default=out)",
             },
         ),
         "tempext": (
             ("-T",),
             {
-                dest: "tempext",
-                default: "temp",
-                help: "extension of temporary files (default=temp)",
+                "dest": "tempext",
+                "default": "temp",
+                "help": "extension of temporary files (default=temp)",
             },
         ),
         "reset": (
             ("-R", "--Reset"),
             {
-                dest: "reset",
-                action: "store_true",
-                help: "recompute outputs, similar as -T out",
+                "dest": "reset",
+                "action": "store_true",
+                "help": "recompute outputs, similar as -T out",
             },
         ),
         "batchname": (
             ("-b", "--batch"),
             {
-                dest: "batchname",
-                default: "00.sample",
-                help: "batch name (default=00.sample)",
+                "dest": "batchname",
+                "default": "00.sample",
+                "help": "batch name (default=00.sample)",
             },
         ),
         "multi": (
             ("-m", "--force-multi"),
             {
-                dest: "multi",
-                action: "store_true",
-                help: "force batch (always print .a before extension)",
+                "dest": "multi",
+                "action": "store_true",
+                "help": "force batch (always print .a before extension)",
             },
         ),
         # testing options
         "timelimit": (
             ("-t", "--time"),
             {
-                dest: "timelimit",
-                default: "3,cpp=1,py=5",
-                help: "set timelimit (default=3,cpp=1,py=5), "
+                "dest": "timelimit",
+                "default": "3,cpp=1,py=5",
+                "help": "set timelimit (default=3,cpp=1,py=5), "
                 + "can be set to unlimited using 0 and "
                 + 'optionally in per language format (e.g. "1.5,py=0,cpp=0.5)"',
             },
@@ -164,37 +168,37 @@ class Parser:
         "warntimelimit": (
             ("--wtime",),
             {
-                dest: "warntimelimit",
-                default: "0",
-                help: "set warn timelimit (default=infinity) which issues warning but does not fail, "
+                "dest": "warntimelimit",
+                "default": "0",
+                "help": "set warn timelimit (default=infinity) which issues warning but does not fail, "
                 + 'can be set in optional per language format (e.g. "1.5,py=5,cpp=0.5)',
             },
         ),
         "memorylimit": (
             ("-m", "--memory"),
             {
-                dest: "memorylimit",
-                help: "set memorylimit (default=infinity)",
-                default: 0,
+                "dest": "memorylimit",
+                "help": "set memorylimit (default=infinity)",
+                "default": 0,
                 "type": float,
             },
         ),
         "wrapper": (
             ("-w", "--wrapper"),
             {
-                dest: "wrapper",
-                nargs: "?",
-                default: False,
-                metavar: "PATH",
-                help: 'use wrapper, default PATH="$WRAPPER"',
+                "dest": "wrapper",
+                "nargs": "?",
+                "default": False,
+                "metavar": "PATH",
+                "help": 'use wrapper, default PATH="$WRAPPER"',
             },
         ),
         "diffcmd": (
             ("-d", "--diff"),
             {
-                dest: "diffcmd",
-                default: None,
-                help: "program which checks correctness of output (default=diff), "
+                "dest": "diffcmd",
+                "default": None,
+                "help": "program which checks correctness of output (default=diff), "
                 + "arguments given to program depends of prefix: "
                 + "       diff $our $theirs,"
                 + "       check $inp $our $theirs,"
@@ -208,158 +212,172 @@ class Parser:
                 "--no-fskip",
             ),
             {
-                dest: "fskip",
-                action: "store_false",
-                help: "dont skip the rest of input files in the same batch after first fail",
+                "dest": "fskip",
+                "action": "store_false",
+                "help": "dont skip the rest of input files in the same batch after first fail",
             },
         ),
         "dupprog": (
             ("--dupprog",),
-            {dest: "dupprog", action: "store_true", help: "keep duplicate programs"},
+            {
+                "dest": "dupprog",
+                "action": "store_true",
+                "help": "keep duplicate programs",
+            },
         ),
         "rustime": (
             ("--no-rustime",),
             {
-                dest: "rustime",
-                action: "store_false",
-                help: "Do not show Real/User/System time statistics",
+                "dest": "rustime",
+                "action": "store_false",
+                "help": "Do not show Real/User/System time statistics",
             },
         ),
         # running options
         "compile": (
             ("--no-compile",),
-            {dest: "compile", action: "store_false", help: "dont try to compile"},
+            {"dest": "compile", "action": "store_false", "help": "dont try to compile"},
         ),
         "sort": (
             ("--no-sort",),
             {
-                dest: "sort",
-                action: "store_false",
-                help: "dont change order of programs",
+                "dest": "sort",
+                "action": "store_false",
+                "help": "dont change order of programs",
             },
         ),
         "execute": (
             ("-x", "--execute"),
             {
-                dest: "execute",
-                action: "store_true",
-                help: "treat programs as bash commands. Dont try to do something smart "
+                "dest": "execute",
+                "action": "store_true",
+                "help": "treat programs as bash commands. Dont try to do something smart "
                 + "as compiling",
             },
         ),
         "pythoncmd_gen": (
             ("--pythoncmd",),
             {
-                dest: "pythoncmd",
-                default: "pypy3",
-                help: "what command is used to execute python, e.g. `python3` or `pypy3` (default=pypy3)",
+                "dest": "pythoncmd",
+                "default": "pypy3",
+                "help": "what command is used to execute python, e.g. `python3` or `pypy3` (default=pypy3)",
             },
         ),
         "pythoncmd_test": (
             ("--pythoncmd",),
             {
-                dest: "pythoncmd",
-                default: "python3",
-                help: "what command is used to execute python, e.g. `python3` or `pypy3` (default=python3)",
+                "dest": "pythoncmd",
+                "default": "python3",
+                "help": "what command is used to execute python, e.g. `python3` or `pypy3` (default=python3)",
             },
         ),
         "threads_gen": (
             ("-j", "--threads"),
             {
-                dest: "threads",
-                default: 6,
-                help: "how many threads to use (default=6)",
+                "dest": "threads",
+                "default": 6,
+                "help": "how many threads to use (default=6)",
                 "type": int,
             },
         ),
         "threads_test": (
             ("-j", "--threads"),
             {
-                dest: "threads",
-                default: 4,
-                help: "how many threads to use (default=4)",
+                "dest": "threads",
+                "default": 4,
+                "help": "how many threads to use (default=4)",
                 "type": int,
             },
         ),
         # verbosing
         "colorful": (
             ("-B", "--boring"),
-            {dest: "colorful", action: "store_false", help: "turn colors off"},
+            {"dest": "colorful", "action": "store_false", "help": "turn colors off"},
         ),
         "colortest": (
             ("--colortest",),
-            {dest: "colortest", action: "store_true", help: "test colors and exit"},
+            {
+                "dest": "colortest",
+                "action": "store_true",
+                "help": "test colors and exit",
+            },
         ),
         "quiet": (
             ("-q", "--quiet"),
             {
-                dest: "quiet",
-                action: "store_true",
-                help: "dont let subprograms print stuff",
+                "dest": "quiet",
+                "action": "store_true",
+                "help": "dont let subprograms print stuff",
             },
         ),
         "Quiet": (
             ("-Q", "--Quiet"),
-            {dest: "Quiet", action: "store_true", help: "dont print anything"},
+            {"dest": "Quiet", "action": "store_true", "help": "dont print anything"},
         ),
         "stats": (
             ("-s", "--statistics"),
             {
-                dest: "deprecated",
-                action: "append_const",
-                const: "statistics",
-                help: "print statistics (deprecated)",
+                "dest": "deprecated",
+                "action": "append_const",
+                "const": "statistics",
+                "help": "print statistics (deprecated)",
             },
         ),
         "nostats": (
             ("--no-statistics",),
-            {dest: "stats", action: "store_false", help: "dont print statistics"},
+            {"dest": "stats", "action": "store_false", "help": "dont print statistics"},
         ),
         # cleanup
         "cleartemp": (
             ("-k", "--keep-temp"),
             {
-                dest: "cleartemp",
-                action: "store_false",
-                help: "dont remove temporary files after finishing",
+                "dest": "cleartemp",
+                "action": "store_false",
+                "help": "dont remove temporary files after finishing",
             },
         ),
         "clearbin": (
             ("--clear-bin",),
             {
-                dest: "clearbin",
-                action: "store_true",
-                help: "remove binary files after finishing",
+                "dest": "clearbin",
+                "action": "store_true",
+                "help": "remove binary files after finishing",
             },
         ),
         "clearinput": (
             ("-k", "--keep-inputs"),
             {
-                dest: "clearinput",
-                action: "store_false",
-                help: "dont remove old input files. Samples are never removed",
+                "dest": "clearinput",
+                "action": "store_false",
+                "help": "dont remove old input files. Samples are never removed",
             },
         ),
         # what to do
-        "programs": (("programs",), {nargs: "+", help: "list of programs to be run"}),
+        "programs": (
+            ("programs",),
+            {"nargs": "+", "help": "list of programs to be run"},
+        ),
         "description": (
             ("description",),
             {
-                nargs: "?",
-                help: "recipe for inputs. If not provided, read it from stdin.",
+                "nargs": "?",
+                "help": "recipe for inputs. If not provided, read it from stdin.",
             },
         ),
         "gencmd": (
             ("-g", "--gen"),
             {
-                dest: "gencmd",
-                default: "gen",
-                help: "generator used for generating inputs (default=gen)",
+                "dest": "gencmd",
+                "default": "gen",
+                "help": "generator used for generating inputs (default=gen)",
             },
         ),
         "task": (
             ("task",),
-            {nargs: "?", help: "task statement. If not provided, read it from stdin."},
+            {
+                "nargs": "?",
+                "help": "task statement. If not provided, read it from stdin.",
+            },
         ),
     }
 
