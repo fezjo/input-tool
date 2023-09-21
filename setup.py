2da830c85f4c930b13754f4b1f3bb7930798aef1
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-09-21 19:47:56 +0200

    Update setup for pip installation

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@ from setuptools import setup, find_packages
 
 
 SCRIPTS = [
-    'input_tool/input-generator',
-    'input_tool/input-sample',
-    'input_tool/input-tester'
+    "input_tool/input-generator",
+    "input_tool/input-sample",
+    "input_tool/input-tester",
 ]
 
-DESCRIPTION = ('Tool which simplifies creating and testing inputs for'
-               ' programming contests.')
+DESCRIPTION = (
+    "Tool which simplifies creating and testing inputs for programming contests."
+)
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='input-tool',
-    version='1.0.0',
+    name="input-tool",
+    version="1.2.0",
+    author="jablkoj (Ján Hozza), fezjo (Jozef Číž)",
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     scripts=SCRIPTS,
     classifiers=[
