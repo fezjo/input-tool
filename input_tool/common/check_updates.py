40e60eefa37a5305a595df658f855434e32e714d
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-09-25 16:09:01 +0200

    Change update message

--- input_tool/common/check_updates.py
+++ input_tool/common/check_updates.py
@@ -5,7 +5,7 @@
 import requests
 from importlib.metadata import version
 
-from input_tool.common.messages import warning
+from input_tool.common.messages import warning, Color
 
 REPO_OWNER = "fezjo"
 REPO_NAME = "input-tool"
@@ -33,14 +33,16 @@ def check_for_updates():
         if latest_version != current_version:
             pip_command = PIP_UPDATE_TEMPLATE.format(REPO_OWNER, REPO_NAME)
             releases_url = GITHUB_RELEASES_URL_TEMPLATE.format(REPO_OWNER, REPO_NAME)
+            effect, noeffect = Color("underlined"), Color("nounderlined")
             warning(
-                f"Newer version {latest_version} is available. "
-                f"You are currently using {current_version}."
+                f"Current `input-tool` version {current_version} is different from\n"
+                f"the latest available version {latest_version}. You can upgrade by running\n"
+                f"{effect}{pip_command}{noeffect}\n"
+                f"or by downloading the latest version from\n"
+                f"{effect}{releases_url}{noeffect}"
             )
-            warning(f"You can upgrade by running: `{pip_command}`")
-            warning(f"or by downloading the latest version from {releases_url}")
     except Exception as e:
-        warning(f"Could not check for updates! An error occurred: {e}")
+        warning(f"Could not check for updates! An error occurred:\n{e}")
 
 
 if __name__ == "__main__":
