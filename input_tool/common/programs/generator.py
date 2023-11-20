653301c78d48d5ceda0b97abd25a2dda2da7c7b7
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-11-20 14:31:03 +0100

    Unlimit stack size for generator

--- input_tool/common/programs/generator.py
+++ input_tool/common/programs/generator.py
@@ -1,6 +1,7 @@
 # © 2014 jano <janoh@ksp.sk>
 # © 2022 fezjo
 import subprocess
+from input_tool.common.commands import Config
 
 from input_tool.common.messages import Status
 from input_tool.common.programs.program import Program
@@ -11,7 +12,12 @@ class Generator(Program):
         return (-4, 0, self.name)
 
     def generate(self, ifile: str, text: str) -> Status:
-        cmd = f"{self.run_cmd} > {ifile}"
+        osc = Config.os_config
+        ulimit_cmd = (
+            f"{osc.cmd_ulimit} -m {osc.mem_unlimited}; "
+            f"{osc.cmd_ulimit} -s {osc.mem_unlimited}"
+        )
+        cmd = f"{ulimit_cmd}; {self.run_cmd} > {ifile}"
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
         p.communicate(str.encode(text))
         return Status.exc if p.returncode else Status.ok
